dee63814e8d6a2721c9127c0b69bfae68f255655
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-17 18:59:41 +0200

    Added validation and searchability, also short_description method

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,5 +1,14 @@
 class Phrase < ActiveRecord::Base
   attr_accessible :title, :description
+  validates_uniqueness_of :title
+  validates_presence_of :title
   extend FriendlyId
   friendly_id :title, use: :slugged
+  def short_description
+  	return self.description.split('.')[0].to_s
+  end
+  searchable do 
+  	text :title, :boost => 5
+  	text :description
+  end
 end
