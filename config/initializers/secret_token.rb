292497f426fcb656bdf8a4ab82112ab6cacb07e4
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-17 10:16:07 +0200

    Generated Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppuga::Application.config.secret_token = 'b44b399beae872c98f24e5029cb03a2b6789e7bf7e96cb1e92225906f00611517aba4746565a76bd6416e1393bb5c5afe37fafb948e941e0428e435172603d19'
