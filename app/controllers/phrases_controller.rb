ecf01099434e991647d011ff7149102d2118d7ca
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-17 19:00:06 +0200

    Added searchability, filled create method

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,10 +1,20 @@
 class PhrasesController < ApplicationController
   expose(:phrase)
-  expose(:phrases)
-  
+  expose(:phrases) {
+  	p = Phrase.search {fulltext params[:search] do
+	  	phrase_fields :title => 5.0
+	  	phrase_fields :description => 3.0
+	  	phrase_slop 1
+	  	end
+	  }
+	p.results
+  }
+
   respond_to :html
 
   def create
+  	phrase.save
+  	redirect_to phrase
   end
 
   def update
