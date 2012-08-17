82215b67d6df47c59554ca23249b32e5d88c4301
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-17 17:08:50 +0200

    Added conversion from Phrase to Revision, some markup to show phrases

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,3 +1,11 @@
 class Revision < ActiveRecord::Base
   attr_accessible :author_id, :description, :phrase_id, :title
+
+  def self.from_phrase (phrase)
+    rev = self.new
+    rev.description = phrase.description
+    rev.title = phrase.title
+    rev.phrase_id = phrase.id
+    return rev
+  end
 end
