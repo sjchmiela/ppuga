fd0578aea75f7a1b332f7cb6e6a843fbfd9d7247
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-18 11:27:55 +0200

    Added allowed tags to sanitizer

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Ppuga
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
   end
 end
