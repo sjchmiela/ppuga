8577ac489fa98e7ce0a754693f534bf39049433d
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-18 11:26:26 +0200

    Added shorten_description method

--- app/helpers/phrases_helper.rb
+++ app/helpers/phrases_helper.rb
@@ -1,2 +1,5 @@
 module PhrasesHelper
+	def shorten_description (p)
+		return strip_tags(p.rendered_description).split('.')[0].to_s
+	end
 end
--- app/helpers/revisions_helper.rb
+++ app/helpers/revisions_helper.rb
@@ -1,2 +1,5 @@
 module RevisionsHelper
+	def shorten_description (r)
+		return strip_tags(r.rendered_description).split('.')[0].to_s
+	end
 end
