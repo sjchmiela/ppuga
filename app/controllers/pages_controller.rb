0ce19c8ee463457f003eaa36a1d581032df56e38
Stanisław Chmiela <sjchmiela@gmail.com>
2012-08-19 19:18:25 +0200

    Respond only to HTML

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 class PagesController < ApplicationController
+  respond_to :html
   def sissy
   	responds = [
   		"Szkoda, ale było miło.",
--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -2,4 +2,5 @@ class RevisionsController < ApplicationController
 expose(:phrase)
 expose(:revisions) {phrase.revisions.includes(:author).order('created_at desc')}
 expose(:revision)
+respond_to :html
 end
