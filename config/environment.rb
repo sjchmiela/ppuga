878535ccce7f1edead1921091c4a8569322cebb4
Stanisław Chmiela <sjchmiela@gmail.com>
2012-09-01 23:16:14 +0200

    Changed Mysql reference to Mysql2

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Patch Mysql adapter to default to MyISAM instead of InnoDB
-require 'active_record/connection_adapters/mysql_adapter'
+# Patch Mysql2 adapter to default to MyISAM instead of InnoDB
+require 'active_record/connection_adapters/mysql2_adapter'
 module ActiveRecord
   module ConnectionAdapters
-    class MysqlAdapter
+    class Mysql2Adapter
       def create_table(table_name, options = {}) #:nodoc:
         super(table_name, options.reverse_merge(:options => "ENGINE=MyISAM"))
       end
