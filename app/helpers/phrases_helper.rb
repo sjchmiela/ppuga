7f10306db1d30e239484bd0b1186a928cbb213d0
Stanisław Chmiela <sjchmiela@gmail.com>
2012-09-23 15:35:46 +0200

    Proper classes

--- app/helpers/phrases_helper.rb
+++ app/helpers/phrases_helper.rb
@@ -2,9 +2,9 @@ module PhrasesHelper
 
 	def search_result (title, desc, url, wiki = false, group_slug = '')
 		if !wiki
-			cont = "<div class='search-result'><h3>#{title} <figure class='#{group_slug}'></figure></h3><p class='search-description'>#{desc}...</div>"
+			cont = "<div class='search-result'><h3>#{title} <figure class='#{group_slug}-search'></figure></h3><p class='search-description'>#{desc}...</div>"
 		else
-			cont = "<div class='search-result'><h3>#{title} <figure class='wikipedia-letter'></figure></h3><p class='search-description'>#{desc}...</div>"
+			cont = "<div class='search-result'><h3>#{title} <figure class='wikipedia-search'></figure></h3><p class='search-description'>#{desc}...</div>"
 		end
 		return link_to (raw (cont)), url, :class => 'search-link'
 	end
