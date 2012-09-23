4601eb8f55db70d50129c7ee1a16fb8eaa3bcbeb
Stanisław Chmiela <sjchmiela@gmail.com>
2012-09-23 15:04:20 +0200

    Join, figures

--- app/helpers/phrases_helper.rb
+++ app/helpers/phrases_helper.rb
@@ -1,8 +1,8 @@
 module PhrasesHelper
 
-	def search_result (title, desc, url, wiki = false)
+	def search_result (title, desc, url, wiki = false, group_slug = '')
 		if !wiki
-			cont = "<div class='search-result'><h3>#{title}</h3><p class='search-description'>#{desc}...</div>"
+			cont = "<div class='search-result'><h3>#{title} <figure class='#{group_slug}'></figure></h3><p class='search-description'>#{desc}...</div>"
 		else
 			cont = "<div class='search-result'><h3>#{title} <figure class='wikipedia-letter'></figure></h3><p class='search-description'>#{desc}...</div>"
 		end
