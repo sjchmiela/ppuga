034d83f5adee92fdc2e4cf5ac2b4cc70975f8885
Stanisław Chmiela <sjchmiela@gmail.com>
2012-09-29 12:26:31 +0200

    Changed SQL to proper

--- db/migrate/20120929101925_add_full_text_index_to_slug_in_phrases.rb
+++ db/migrate/20120929101925_add_full_text_index_to_slug_in_phrases.rb
@@ -1,5 +1,5 @@
 class AddFullTextIndexToSlugInPhrases < ActiveRecord::Migration
   def change
-  	execute "ALTER TABLE phrases ADD FULLTEXT (slug)"
+  	execute "ALTER TABLE phrases ADD FULLTEXT (slug, title, description);"
   end
 end
