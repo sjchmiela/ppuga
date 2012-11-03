d0ba059c7eecdf9828982b5b0b1413bb22c59d7b
Stanisław Chmiela <sjchmiela@gmail.com>
2012-11-03 15:13:26 +0100

    Completed json phrases

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,29 @@
 function search() {
-	var keyword = $("#search").val();
+	var keyword = $("#search_field").val();
 	$("header.search").text("Wyszukiwanie: „"+keyword+"”");
-	$.getJSON('/phrases/search.json?utf8=✓&search='+keyword, function(data)
-	{
+	$.getJSON('/phrases/search.json?utf8=✓&search='+keyword, function(data)	{
 		$(".search-result").remove();
 		$.each(data.results, function(i,item)
 		{
-			$("#search-results").append('<a href="/phrases/'+item.slug+'" class="search-result element">'+item.title+'</a>')
+			$("#search-results").append('<a href="/phrases/'+item.slug+'" class="search-result element" onClick="javascript:visit(event, this.href)">'+item.title+'</a>')
 		});
 		$.each(data.wikipedia, function(i,item)
 		{
-			$('#search-results').append('<a href="'+item.href+'" class="search-result element">'+item.title+'</a>')
+			$('#search-results').append('<a href="'+item.href+'" class="search-result element" onClick="javascript:visit(event, this.href)">'+item.title+'</a>')
 		});
 	});
 }
 
+function visit(event, url) {
+	event.preventDefault();
+	$.getJSON(url+'.json', (function(data)	{
+		document.title = data.title+" - PPuga - przedsiębiorczy słownik";
+		history.pushState(data.title, document.title, url);
+		$("#group").html('<a href="'+data.group_link+'">'+data.group+'</a>');
+		$("article.scroll").html('<header><h1>'+data.title+'</h1></header><section>'+data.description+'</section>');
+	}));
+}
+
 $().ready(function () {
-	$("form").submit(function(event) { event.preventDefault(); search();})
+	$("form").submit(function(event) { event.preventDefault();});
 });
